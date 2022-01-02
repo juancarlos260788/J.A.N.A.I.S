c558f7f4c5e98d634d70c3747c4555c1133c9c5f
juancarlos260788 <72200099+juancarlos260788@users.noreply.github.com>
2022-01-02 14:18:28 +1000

    Update and rename main.py to speechrecognition.py

---
+++ speechrecognition.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+from gtts import gTTS
+import os
+
+voice = ""
+while True:
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            audio = r.listen(source)
+            text = r.recognize_google(audio)
+            print(text)
+            if text == "stop":
+                break
+            text = r.recognize_google(audio)
+            voice =voice +str(text)
+        except:
+            print("say something...")
+hr =gTTS(text=voice, Lang='en', slow=False)
+hr.save("1.wav")
+
+
+
+
+
